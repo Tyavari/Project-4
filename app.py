79ec03edb8cf748a5bb7cceb95f82c3c8274a6b2
tatianafrattale <tatianafrattale@gmail.com>
2021-11-20 15:53:54 -0500

    all buttons

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # import necessary libraries
-from models import create_classes
+# from models import create_classes
 import pandas as pd
-import os
 import joblib
 from flask import (
     Flask,
@@ -13,63 +12,70 @@ from flask import (
 #################################################
 # Flask Setup
 #################################################
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 #################################################
 # Database Setup
 #################################################
 
-model = joblib.load("RandomForest1.joblib")
-# create route that renders index.html template
+model = joblib.load("RandomForest2.joblib")
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("form2.html")
 
 
-# Query the database and send the jsonified results
 @app.route("/send", methods=["GET", "POST"]) #probably won't use get
 def send():
     if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
-        X_test = pd.DataFrame()
-        result = model.predict(X_test)  
-        return render_template("summary.html", result=result) #html needs to be called summary 
-    return render_template("form.html") 
+        neighborhood = request.form["neighborhood"]
+        building_type = request.form["BuildingType"]
+        zip = request.form["ZipCode"]
+        units = request.form["totalUnits"]
+        sqft = request.form["sqft"]
+        year = request.form["year"]
+        # X_test = pd.read_csv("x_test.csv")
+        # result = model.predict(X_test)  
+        # return render_template("summary.html", result=result) #html needs to be called summary 
+        return (f'{neighborhood} {building_type} {zip} {units} {sqft} {year}')
+    X_test = pd.read_csv("x_test.csv")
+    result = model.predict(X_test)  
+    return (f'{result}')
 
 # will only end up using send route or api route
 
-@app.route("/api/pals")
-def pals():
-     name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
-        X_test = pd.DataFrame()
-        result = model.predict(X_test)
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+# @app.route("/api/pals")
+# def pals():
+#      name = request.form["petName"]
+#         lat = request.form["petLat"]
+#         lon = request.form["petLon"]
+#         X_test = pd.DataFrame()
+#         result = model.predict(X_test)
+#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
 
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
+#     hover_text = [result[0] for result in results]
+#     lat = [result[1] for result in results]
+#     lon = [result[2] for result in results]
 
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 50,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
+#     pet_data = [{
+#         "type": "scattergeo",
+#         "locationmode": "USA-states",
+#         "lat": lat,
+#         "lon": lon,
+#         "text": hover_text,
+#         "hoverinfo": "text",
+#         "marker": {
+#             "size": 50,
+#             "line": {
+#                 "color": "rgb(8,8,8)",
+#                 "width": 1
+#             },
+#         }
+#     }]
 
-    return jsonify(pet_data)
+#     return jsonify(pet_data)
 
 
 if __name__ == "__main__":
