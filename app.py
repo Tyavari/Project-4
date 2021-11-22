76b18b82670d9c385f30c68856085bc08f397734
tatianafrattale <tatianafrattale@gmail.com>
2021-11-22 18:02:30 -0500

    cleaning code

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def home():
     return render_template("form2.html")
 
 
-@app.route("/send", methods=["GET", "POST"]) #probably won't use get
+@app.route("/send", methods=["GET", "POST"]) 
 def send():
     if request.method == "POST":
         neighborhood = request.form["neighborhood"]
@@ -36,11 +36,6 @@ def send():
         units = request.form["totalUnits"]
         sqft = request.form["sqft"]
         year = request.form["year"]
-        # X_test = pd.read_csv("x_test.csv")
-        # result = model.predict(X_test)  
-        # return render_template("summary.html", result=result) #html needs to be called summary 
-
-        # Start of Claudia changes
 
         # Make sqft input numeric based on binning options
         if "-" in sqft:
@@ -74,51 +69,8 @@ def send():
 
 
         result = (model.predict(inputdf))[0]  
-        #tohtml = [dic, result]
-        #tohtml = {"dic": dic, "results": result}
-        #return render_template('summary.html', info=result[0])
-        # End of Claudia changes
-
-
 
         return (f'The Price Prediction is: ${result:.2f}')
-    # X_test = pd.read_csv("x_test.csv")
-    # result = model.predict(X_test)  
-    # return (f'{result}')
-
-# will only end up using send route or api route
-
-# @app.route("/api/pals")
-# def pals():
-#      name = request.form["petName"]
-#         lat = request.form["petLat"]
-#         lon = request.form["petLon"]
-#         X_test = pd.DataFrame()
-#         result = model.predict(X_test)
-#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
-
-#     hover_text = [result[0] for result in results]
-#     lat = [result[1] for result in results]
-#     lon = [result[2] for result in results]
-
-#     pet_data = [{
-#         "type": "scattergeo",
-#         "locationmode": "USA-states",
-#         "lat": lat,
-#         "lon": lon,
-#         "text": hover_text,
-#         "hoverinfo": "text",
-#         "marker": {
-#             "size": 50,
-#             "line": {
-#                 "color": "rgb(8,8,8)",
-#                 "width": 1
-#             },
-#         }
-#     }]
-
-#     return jsonify(pet_data)
-
 
 if __name__ == "__main__":
     app.run()
